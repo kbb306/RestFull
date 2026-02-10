0f0e86322484c58eeb299a119eef90a2daa1ca32
kbb306 <kylerbbelshaw@gmail.com>
2026-02-10 12:30:59 -0700

    Rewrote Alarm class again

--- app/src/main/java/com/example/restfull/RestFull.kt
+++ app/src/main/java/com/example/restfull/RestFull.kt
@@ -1,32 +1,22 @@
 package com.example.restfull
 
-class Alarm(Name : String, threshold: Int, sound : String) {
-    init {
-        require(threshold > 0 && threshold >= 100)
-        require(!Name.equals(""))
+class Alarm {
+    var name : String? = null
+    var threshold : Int? = null
+    var sound : String? = null
+
+    constructor(name:String, threshold:Int, sound:String) {
+        require(name != "" && threshold in (0..100))
+        this.name = name
+        this.threshold = threshold
+        this.sound = sound
     }
-    var name = Name
-        set(value) {
-            when {
-                value.equals("") -> {
-                    //Raise some exception or create "untitled x"
-                }
-                else -> field = value
-            }
-        }
-    var Threshold: Int = threshold
-        set(value)  {
-            field = when {
-                value < 0 -> 0
-                value > 100 -> 100
-                else -> value
-            }
-        }
 
-    var Sound = sound
-
-    fun battcheck(current : Int) : Boolean {
-        // API Bullshit here
-        return TODO("Provide the return value")
+    fun check() : String? {
+        var batt : Int = TODO("API Call here")
+        return when {
+            this.threshold!! >= batt -> this.sound
+            else -> "not yet"
+        }
     }
 }
