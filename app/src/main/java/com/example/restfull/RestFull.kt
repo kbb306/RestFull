e0af690a4f922b344ba88ce342810c00a87126fa
kbb306 <kylerbbelshaw@gmail.com>
2026-02-11 10:17:41 -0700

    Added volume attribute and removed misplaced check() function.

--- app/src/main/java/com/example/restfull/RestFull.kt
+++ app/src/main/java/com/example/restfull/RestFull.kt
@@ -4,19 +4,15 @@ class Alarm {
     var name : String? = null
     var threshold : Int? = null
     var sound : String? = null
+    var volume : Int? = null
 
-    constructor(name:String, threshold:Int, sound:String) {
+    constructor(name:String, threshold:Int, sound:String, volume:Int) {
         require(name != "" && threshold in (0..100))
         this.name = name
         this.threshold = threshold
         this.sound = sound
+        this.volume = volume
     }
 
-    fun check() : String? { // Consider moving to Viewmodel?
-        var batt : Int = TODO("API Call here")
-        return when {
-            this.threshold!! >= batt -> this.sound
-            else -> "not yet"
-        }
-    }
+
 }
