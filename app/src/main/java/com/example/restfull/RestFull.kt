3d269f82f31fd1b1543bb44e2b5a081ad1ac6167
kbb306 <kylerbbelshaw@gmail.com>
2026-02-11 11:30:48 -0700

    Added volume and on attributes to model

--- app/src/main/java/com/example/restfull/RestFull.kt
+++ app/src/main/java/com/example/restfull/RestFull.kt
@@ -6,12 +6,15 @@ class Alarm {
     var sound : String? = null
     var volume : Int? = null
 
-    constructor(name:String, threshold:Int, sound:String, volume:Int) {
+    var on : Boolean? = null
+
+    constructor(name:String, threshold:Int, sound:String, volume:Int, on: Boolean) {
         require(name != "" && threshold in (0..100))
         this.name = name
         this.threshold = threshold
         this.sound = sound
         this.volume = volume
+        this.on = on
     }
 
 
