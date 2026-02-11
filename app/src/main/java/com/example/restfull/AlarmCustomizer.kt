1d22e9d01f627991736fca9f2a94609024acbb6a
kbb306 <kylerbbelshaw@gmail.com>
2026-02-10 17:17:03 -0700

    Rewrote without using MainACtivity as a template

--- app/src/main/java/com/example/restfull/AlarmCustomizer.kt
+++ app/src/main/java/com/example/restfull/AlarmCustomizer.kt
@@ -1,13 +1,27 @@
 package com.example.restfull
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.example.restfull.databinding.FragmentCustalarmBinding
 
 class AlarmCustomizer : DialogFragment() {
-    private lateinit var binding: FragmentCustalarmBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = FragmentCustalarmBinding.inflate(layoutInflater)
+    private var _binding: FragmentCustalarmBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCustalarmBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Bindings here
     }
 }
