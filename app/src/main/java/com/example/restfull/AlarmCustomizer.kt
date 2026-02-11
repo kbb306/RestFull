8ed244db9a7e26791175e6815db83e2f9d40014a
kbb306 <kylerbbelshaw@gmail.com>
2026-02-10 20:40:34 -0700

    Todo - More concise solution with extension function.

--- app/src/main/java/com/example/restfull/AlarmCustomizer.kt
+++ app/src/main/java/com/example/restfull/AlarmCustomizer.kt
@@ -1,11 +1,13 @@
 package com.example.restfull
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.example.restfull.databinding.FragmentCustalarmBinding
+import android.text.TextWatcher
 
 class AlarmCustomizer : DialogFragment() {
     private var _binding: FragmentCustalarmBinding? = null
@@ -22,6 +24,28 @@ class AlarmCustomizer : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Bindings here
+        binding.custompercent.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
