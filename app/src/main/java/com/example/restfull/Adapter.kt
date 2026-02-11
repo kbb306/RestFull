2f0d59119f0acfa5dcef09d56cc17d3cc970a9fa
kbb306 <kylerbbelshaw@gmail.com>
2026-02-11 00:55:53 -0700

    Created prototype Adapter class

--- app/src/main/java/com/example/restfull/Adapter.kt
+++ app/src/main/java/com/example/restfull/Adapter.kt
@@ -2,11 +2,35 @@ package com.example.restfull
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.restfull.RestFull
 
-class Adapter {
+// Todo Convert to viewBinding
+class Adapter(private val alarmList: List<Alarm>) :
+    RecyclerView.Adapter<Adapter.Viewholder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Adapter.Viewholder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.alarm_card,
+            parent,false)
+            return Viewholder(view)
+        }
 
-}
+    override fun onBindViewHolder(holder: Adapter.Viewholder, position: Int) {
+        val alarmItem = alarmList[position]
+        holder.alarmName.text = alarm.name
+        holder.alarmPercent.text = alarm.threshold.toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return alarmList.size
+    }
+    class Viewholder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val alarmName : TextView =
+            itemView.findViewById(R.id.alarmname)
+        val alarmPercent : TextView =
+            itemView.findViewById(R.id.custompercent)
+    }
+    }
+
+
+
--- app/src/main/java/com/example/restfull/MainActivity.kt
+++ app/src/main/java/com/example/restfull/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        val recycler = binding.scratchpad
         binding.filler.setOnSeekBarChangeListener { view : View ->
 
         }
