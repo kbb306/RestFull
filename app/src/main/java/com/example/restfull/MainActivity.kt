c44052a287da3b5692d78fdac32f19be65967153
kbb306 <kylerbbelshaw@gmail.com>
2026-02-11 11:30:14 -0700

    Fixed filler binding

--- app/src/main/java/com/example/restfull/MainActivity.kt
+++ app/src/main/java/com/example/restfull/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.restfull
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
+import android.widget.SeekBar
 import androidx.core.view.WindowInsetsCompat
 import com.example.restfull.databinding.ActivityMainBinding
 import androidx.activity.viewModels
@@ -17,9 +18,21 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         val recycler = binding.scratchpad
-        binding.filler.setOnSeekBarChangeListener { view : View ->
+        binding.filler.setOnSeekBarChangeListener(object  : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(
+                seekBar: SeekBar?,
+                progress: Int,
+                fromUser: Boolean
+            ) {
+                viewModel
+            }
 
-        }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
 
     }
 }
