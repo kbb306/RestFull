2e442cc4f7c5475c0b822201f3741a54b0531838
kbb306 <kylerbbelshaw@gmail.com>
2026-02-11 11:31:07 -0700

    Began work on viewmodel

--- app/src/main/java/com/example/restfull/RestFullViewModel.kt
+++ app/src/main/java/com/example/restfull/RestFullViewModel.kt
@@ -1,8 +1,15 @@
 package com.example.restfull
 import androidx.lifecycle.ViewModel
 class RestFullViewModel : ViewModel() {
-    fun add(threshold : Int, name : String, sound : String) : String {
-        val x : Alarm  = Alarm(name,threshold,sound)
-        return x.Name
+    val alarmList: MutableList<Alarm> = arrayListOf(Alarm("Default",100,"Bells",75, true))
+    fun add(threshold : Int, name : String, sound : String, volume : Int, on : Boolean)  {
+        val x : Alarm  = Alarm(name,threshold,sound,volume, on)
+        alarmList.add(x)
     }
+
+    fun onoff(pos : Int,) {
+        alarmList[pos].on = !(alarmList[pos].on)!!
+    }
+
+
 }
